bfaa5aa69a544daec3ce7c8ccc6a77146effb409
kaifumi <kaifumi1001@gmail.com>
2020-06-15 02:26:54 +0000

    [Update]計画時間を0時間から設定可能に変更 #81

--- db/schema.rb
+++ db/schema.rb
@@ -81,7 +81,7 @@ ActiveRecord::Schema.define(version: 2020_05_26_073800) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "deleted_at"
-    t.index ["email"], name: "index_users_on_email", unique: true, where: "deleted_at IS NULL"
+    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
